4ad0212831383684de271ce76e41a89d69baa6a2
Altti Tammi <altti.tammi@gmail.com>
2013-02-24 19:46:50 +0200

    Bar code separated to be used in both the old cpubar and the new netbar

--- cpubar.hs
+++ cpubar.hs
@@ -3,13 +3,15 @@
  -}
 
 {-# LANGUAGE BangPatterns #-}
-import Control.Monad (liftM, forever)
+import Control.Monad (forever)
 import Control.Concurrent (threadDelay)
 import Data.List (isPrefixOf)
 import System.IO
 import Data.IORef
+import BrailleBar
 
-meterlength = 16
+meterchars = 16
+meterdots = 2*meterchars
 
 low="#13ad2f"
 medium="#feb500"
@@ -18,7 +20,7 @@ delay=1000000
 
 main = do
   mapM_ (flip hSetBuffering NoBuffering) [stdin, stdout]
-  stat <- newIORef ([[]] :: [[Double]])
+  stat <- newIORef ([[]] :: [[Int]])
   !oldstat <- cpuData
   writeIORef stat oldstat
   forever $ do
@@ -28,35 +30,29 @@ main = do
     let !load = zipWith coreLoad oldstat newstat
     writeIORef stat newstat
     let ll = length load
-    let load' = take 4 $ load ++ (replicate (4 - ll) 0.0)
-    let cpus = cpuBar meterlength load'
-    let (lowstr, temp) = splitAt (div meterlength 2) cpus
-    let (medstr, histr) = splitAt (div meterlength 4) temp
+    let load' = map (numDots meterdots) $ take 4 $ load ++ (replicate (4 - ll) 0.0)
+    let cpustring = brailleBar (meterdots) load'
+    let (lowstr, temp) = splitAt (div meterchars 2) cpustring
+    let (medstr, histr) = splitAt (div meterchars 4) temp
     putStr "CPU"
     putChar $ ['\10248','\10264','\10296','\10424']!!(ll-1)
-    putStr $ "<fc=" ++ low ++ ">" ++ lowstr ++ "</fc><fc=" ++ medium ++ ">" ++ medstr ++ "</fc><fc=" ++ high ++ ">" ++ histr ++ "</fc>"
+    putStr $ concat
+		  [ "<fc=", low, ">", lowstr, "</fc>"
+			, "<fc=", medium, ">", medstr, "</fc>"
+			, "<fc=", high, ">", histr, "</fc>"
+			]
     putChar $ ['\10241','\10243','\10247','\10311']!!(ll-1)
     putChar '\n'
     hFlush stdout
 
-cpuData :: IO [[Double]]
+cpuData :: IO [[Int]]
 cpuData = do
   f <- readFile "/proc/stat"
-  return $ map (map read.tail.words) . takeWhile ("cpu" `isPrefixOf`) . tail . lines $ f
+  return $ map (map read . take 4 . tail . words) . takeWhile ("cpu" `isPrefixOf`) . tail . lines $ f
 
-coreLoad :: Fractional a => [a] -> [a] -> a
-coreLoad (a1:a2:a3:a4:_) (b1:b2:b3:b4:_) = k / (k + b4 - a4)
+coreLoad :: (Integral a, Fractional b) => [a] -> [a] -> b
+coreLoad (a1:a2:a3:a4:_) (b1:b2:b3:b4:_) = (fromIntegral k) / (fromIntegral $ k + b4 - a4)
   where k = b1+b2+b3-a1-a2-a3
 
-cpuBar :: Int -> [Double] -> String
-cpuBar meterlength = br . map (unary totaldots) . map numdots
-  where
-  totaldots = 2*meterlength
-  numdots i = floor $ i*(fromIntegral $ totaldots)+0.5
-  br ([]:_) = []
-  br [(a1:a2:as),(b1:b2:bs),(c1:c2:cs),(d1:d2:ds)] = (toEnum $ 10240+a1+2*b1+4*c1+8*a2+16*b2+32*c2+64*d1+128*d2 :: Char) : (br [as,bs,cs,ds])
-
-unary 0 _ = []
-unary k 0 = replicate k 0
-unary k n = 1 : unary (k-1) (n-1)
+numDots totaldots i = floor $ i*(fromIntegral $ totaldots)+0.5
 
---
+++ netbar.hs
@@ -0,0 +1,55 @@
+{- Copyright : Altti Tammi
+ - License   : BSD3 (See LICENSE)
+ -}
+
+{-# LANGUAGE BangPatterns #-}
+import Control.Monad (forever)
+import Control.Concurrent (threadDelay)
+import System.IO
+import Data.IORef
+import BrailleBar
+
+charsPerMagnitude = 4
+meterlength = 3*2*charsPerMagnitude
+interface = "eth0"
+
+low="#13ad2f"
+medium="#feb500"
+high="#f5010a"
+delay=1000000
+
+main = do
+  mapM_ (flip hSetBuffering NoBuffering) [stdin, stdout]
+  stat <- newIORef ((0,0) :: (Integer, Integer))
+  !oldstat <- netData interface
+  writeIORef stat oldstat
+  forever $ do
+    threadDelay delay
+    newstat <- netData interface
+    oldstat <- readIORef stat
+    writeIORef stat newstat
+    let !load = (usageDots $ fst newstat - fst oldstat, usageDots $ snd newstat - snd oldstat)
+    let netstring = brailleBar meterlength [0,fst load,snd load,0]
+    let (lowstr, temp) = splitAt charsPerMagnitude netstring
+    let (medstr, histr) = splitAt charsPerMagnitude temp
+    putStrLn $ concat 
+			[ interface ++ " "
+			, "<fc=#ffffff>B</fc>"
+			, "<fc=", low, ">", lowstr, "</fc>"
+			, "<fc=#ffffff>K</fc>"
+			, "<fc=", medium, ">", medstr, "</fc>"
+			, "<fc=#ffffff>M</fc>"
+			, "<fc=", high, ">", histr, "</fc>"
+			, "<fc=#ffffff>G</fc>"
+			]
+    hFlush stdout
+
+usageDots :: Integer -> Int
+usageDots n = floor $ log (fromIntegral $ max 1 n) * (fromIntegral $ 2*charsPerMagnitude) / log 1024
+
+netData :: String -> IO (Integer, Integer)
+netData interface = do
+	rx <- readFile $ "/sys/class/net/"++interface++"/statistics/rx_bytes"
+	tx <- readFile $ "/sys/class/net/"++interface++"/statistics/tx_bytes"
+	return (read rx, read tx)
+
